d040e230487a42e648fec5a3d45c6b12b24bd8b9
fsechavarria <fsechavarria@gmail.com>
2020-07-01 18:37:15 -0400

    Linting, moved magic values to variables && changed to new stamina formula

--- src/utils/calculateStamina.js
+++ src/utils/calculateStamina.js
@@ -1,29 +1,36 @@
-import moment from 'moment'
+import moment from 'moment';
 
 export const calculateTimeToFull = (
   stamina = { currentHours: 0, currentMinutes: 0 },
   eventDate = moment().format()
 ) => {
-  const { currentHours, currentMinutes } = stamina
-  const minutesAtReducedGainTime = 40 * 60
-  const minutesAtFull = 41 * 60 + 59
-  const minutesToEvent = moment(eventDate).diff(new Date(), 'minutes')
-  const staminaToMinutes = +currentHours * 60 + +currentMinutes
+  const { currentHours, currentMinutes } = stamina;
+  const minutesAtReducedGainTime = 39 * 60;
+  const minutesAtFull = 41 * 60 + 59;
+  const minutesToEvent = moment(eventDate).diff(new Date(), 'minutes');
+  const staminaToMinutes = +currentHours * 60 + +currentMinutes;
+  const greenStaminaMultiplier = 6;
+  const regularStaminaMultiplier = 3;
+  const extraGreenStaminaMinutes = 10;
+  const minutesToChargeGreen =
+    (minutesAtFull - minutesAtReducedGainTime) * greenStaminaMultiplier + extraGreenStaminaMinutes;
 
-  if (staminaToMinutes >= minutesAtFull) return { message: 'You are already at 41:59 stamina.', date: '' }
+  if (staminaToMinutes >= minutesAtFull) return { message: 'You are already at 41:59 stamina.', date: '' };
 
-  const differenceFromFullAtReduced = (minutesAtFull - staminaToMinutes) * 10
+  const differenceFromFullAtReduced = (minutesAtFull - staminaToMinutes) * greenStaminaMultiplier;
   const minutesToReduced =
-    minutesAtReducedGainTime - staminaToMinutes > 0 ? (minutesAtReducedGainTime - staminaToMinutes) * 3 : 0
+    minutesAtReducedGainTime - staminaToMinutes > 0
+      ? (minutesAtReducedGainTime - staminaToMinutes) * regularStaminaMultiplier
+      : 0;
   const timeToFull =
-    staminaToMinutes >= minutesAtReducedGainTime ? differenceFromFullAtReduced + 10 : minutesToReduced + 1210
+    staminaToMinutes >= minutesAtReducedGainTime
+      ? differenceFromFullAtReduced + extraGreenStaminaMinutes
+      : minutesToReduced + minutesToChargeGreen;
 
-  const hasEnoughTime = minutesToEvent >= timeToFull
-  if (!hasEnoughTime) return { message: 'There is not enough time to recover your stamina.', date: '' }
+  const hasEnoughTime = minutesToEvent >= timeToFull;
+  if (!hasEnoughTime) return { message: 'There is not enough time to recover your stamina.', date: '' };
 
   return {
-    date: moment(eventDate)
-      .subtract(timeToFull, 'minutes')
-      .format('MM-DD-YYYY - hh:mm a')
-  }
-}
+    date: moment(eventDate).subtract(timeToFull, 'minutes').format('MM-DD-YYYY - hh:mm a'),
+  };
+};
